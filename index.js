696a019108257cd976fe20fa5d60b6a8fb3d18a3
jimaa maya <jimaa.maya@gmail.com>
2023-03-21 16:58:22 +0300

    phase-1-functions-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+
+
+function distanceFromHqInBlocks(location) {
+    const headquarters = 42; 
+    const distanceInBlocks = Math.abs(location - headquarters);
+    return distanceInBlocks;
+  }
+  
+
+function distanceFromHqInFeet(location) {
+    const blockLength = 264; 
+    const headquarters = 42; 
+    const distanceInBlocks = Math.abs(location - headquarters);
+    const distanceInFeet = distanceInBlocks * blockLength;
+    return distanceInFeet;
+  }
+
+  function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
